34ab8c267a6255ad282377d711b8395d3025046d
flagarde <lagarde@sjtu.edu.cn>
2020-12-06 20:59:40 +0800

    Version 1.2

--- Changelog.cmake
+++ Changelog.cmake
@@ -1,4 +1,5 @@
 function(changelog)
   cmmm_changes(1.0 "Initial version")
   cmmm_changes(1.1 "Some typos" "Fix a bug when FOLDER is not given")
+  cmmm_changes(1.2 "Some typos" "Fix LatestVersion.cmake downlaoding")
 endfunction()
