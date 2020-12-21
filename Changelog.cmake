a936bad533308b5c96a0531348c51b567a2ef2b8
flagarde <lagarde@sjtu.edu.cn>
2020-12-21 19:56:08 +0800

    Version 1.1

--- Changelog.cmake
+++ Changelog.cmake
@@ -1,5 +1,5 @@
 function(changelog)
   cmmm_changes(0.9 "Pre-version")
   cmmm_changes(1.0 "Initial Version")
-  cmmm_changes(1.1 "Fix some color issues")
+  cmmm_changes(1.1 "Fix some color issues" "Some typos")
 endfunction()
