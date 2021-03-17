becb690dd408833b5b94f07cf2f0b470b3f05b7a
flagarde <francois.lagarde@protonmail.com>
2021-03-17 14:02:40 +0800

    Update Changelog.cmake

--- Changelog.cmake
+++ Changelog.cmake
@@ -1,8 +1,6 @@
-set(CMMM_LATEST_VERSION 1.1)
+set(CMMM_LATEST_VERSION 1.0)
 
 function(changelog)
   cmmm_changes(0.9 "Alpha version")
   cmmm_changes(1.0 "Initial version")
-  cmmm_changes(1.1 "Added different providers for CMakeMM")
-  cmmm_changes(1.2 "Add providers for cmmm_modules_list")
 endfunction()
