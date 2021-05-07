a689f8e12ee9cc0dc02bd05440bd3fd6ed48f49e
flagarde <lagarde@sjtu.edu.cn>
2021-05-07 20:44:08 +0800

    Version 2.0

--- Changelog.cmake
+++ Changelog.cmake
@@ -1,7 +1,8 @@
-set(CMMM_LATEST_VERSION 1.0)
+set(CMMM_LATEST_VERSION 2.0)
 
 function(changelog)
   cmmm_changes(0.9 "Alpha version")
   cmmm_changes(1.0 "Initial version")
   cmmm_changes(1.1 "Default branch is now main not master" "Avoid redownloading modules list")
+  cmmm_changes(2.0 "Supress git and FetchContent dependencies" "${BoldRed}!! Please download the new GetCmakeMM.cmake !!${Reset}")
 endfunction()
